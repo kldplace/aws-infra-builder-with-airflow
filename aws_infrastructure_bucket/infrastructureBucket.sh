594f55057da9a7de465108b677089148b596bcda
kldplace <kaled.workspace@gmail.com>
2024-07-02 02:59:07 +0300

    updating on s3 Bucket script

--- aws_infrastructure_bucket/infrastructureBucket.sh
+++ aws_infrastructure_bucket/infrastructureBucket.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
+# Path to (CloudFormation_yaml) folder
+CLOUDFORMATION_FOLDER_PATH="/home/kld/Github_projects/aws-infra-builder-with-airflow/CloudFormation_yaml/"
 # Creaete [infrastructure-bucket-2024] bucket
 aws s3api create-bucket --bucket infrastructure-bucket-2024 --region me-south-1 --create-bucket-configuration LocationConstraint=me-south-1
 
-# -- PREPARING A PLACE FOR (NETWORKING) JSON FILES --
-# create [NetworkingSetup] folder
-aws s3api put-object --bucket infrastructure-bucket-2024 --key NetworkingSetup/
-# create [json and ymal] folders
-aws s3api put-object --bucket infrastructure-bucket-2024 --key NetworkingSetup/Json/
-aws s3api put-object --bucket infrastructure-bucket-2024 --key NetworkingSetup/Yaml/
-# Upload json file that created a network infrastructure
-aws s3 cp ../CloudFormation_json/step-01-create-VPC.json s3://infrastructure-bucket-2024/NetworkingSetup/Json/step-01-create-VPC.json
-aws s3 cp ../CloudFormation_json/step-02-create-subnets.json s3://infrastructure-bucket-2024/NetworkingSetup/Json/step-02-create-subnets.json
-aws s3 cp ../CloudFormation_json/step-03-create-internetGateway.json s3://infrastructure-bucket-2024/NetworkingSetup/Json/step-03-create-internetGateway.json
-aws s3 cp ../CloudFormation_json/step-04-create-NATgateways.json s3://infrastructure-bucket-2024/NetworkingSetup/Json/step-04-create-NATgateways.json
-aws s3 cp ../CloudFormation_json/step-05-create-routeTables.json s3://infrastructure-bucket-2024/NetworkingSetup/Json/step-05-create-routeTables.json
-aws s3 cp ../CloudFormation_json/step-06-create-securityGroups.json s3://infrastructure-bucket-2024/NetworkingSetup/Json/step-06-create-securityGroups.json
+# Upload yaml files that create our infrastructure
+aws s3 cp $CLOUDFORMATION_FOLDER_PATH/step-01-create-networkingStructure.yaml s3://infrastructure-bucket-2024/step-01-create-networkingStructure.yaml
+aws s3 cp $CLOUDFORMATION_FOLDER_PATH/step-02-create-database.yaml s3://infrastructure-bucket-2024/step-02-create-database.yaml
+aws s3 cp $CLOUDFORMATION_FOLDER_PATH/step-03-create-EFSMountTarget.yaml s3://infrastructure-bucket-2024/step-03-create-EFSMountTarget.yaml
+aws s3 cp $CLOUDFORMATION_FOLDER_PATH/step-04-create-targetGroup-and-loadBalancer.yaml s3://infrastructure-bucket-2024/step-04-create-targetGroup-and-loadBalancer.yaml
+aws s3 cp $CLOUDFORMATION_FOLDER_PATH/step-05-launchTemplate.yaml s3://infrastructure-bucket-2024/step-05-launchTemplate.yaml
+aws s3 cp $CLOUDFORMATION_FOLDER_PATH/step-06-create-autoScalingGroup.yaml s3://infrastructure-bucket-2024/step-06-create-autoScalingGroup.yaml
 
 
 
