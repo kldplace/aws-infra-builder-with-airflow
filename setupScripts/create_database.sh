9141f85bfd893b3e72df90a8de2f9f2341592e83
kldplace <kaled.workspace@gmail.com>
2024-07-01 22:44:31 +0300

    setupScripts/database setup - script fpr RDS database setup is created

--- setupScripts/create_database.sh
+++ setupScripts/create_database.sh
@@ -69,7 +69,7 @@ aws rds create-db-instance \
 
 
 # [step-02-create-database.yaml]
-# YAML templete to create RDS database structure to use it with CloudFormation service
+# YAML template to create RDS database structure to use it with CloudFormation service
 # note: you should execute [step-01-create-networkingStructure.yaml] template before using this template
 cat << EOF > $YAML_FILE
 AWSTemplateFormatVersion: "2010-09-09"
